4962a31336e36bc5b46ac4531afa60c7ec828345
TBringerOHW <1closed1@gmail.com>
2021-05-06 00:58:50 +0300

    - [DebugLogger] Reduced stack trace size for unity.

--- GServer/GServer.RPC/DebugLogger.cs
+++ GServer/GServer.RPC/DebugLogger.cs
@@ -1,24 +1,24 @@
 ﻿using System;
+
 #if UNITY_ENGINE
 using UnityEngine;
 #endif
 
 namespace GServer.RPC
 {
-    public static class DebugLogger
-    {
-        public enum ELogMessageType
-        {
-            Default,
-            Info,
-            Warning,
-            Error,
-        }
-        
-        public static void LogMessage(string message, ELogMessageType messageType)
-        {
-#if UNITY_ENGINE
-            message = $"[{DateTime.Now}] " + message;
+	public static class DebugLogger
+	{
+		public enum ELogMessageType
+		{
+			Default,
+			Info,
+			Warning,
+			Error,
+		}
+
+		private static void LogMessage(string message, ELogMessageType messageType)
+		{
+		#if UNITY_ENGINE
             switch (messageType)
             {
                 case ELogMessageType.Default:
@@ -36,31 +36,114 @@ namespace GServer.RPC
                 default:
                     throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
             }
-#else
-            message = $"[{messageType}] [{DateTime.Now}] {message}";
-            Console.WriteLine(message);
-#endif
-        }
+		#else
+			message = $"[{messageType}] [{DateTime.Now}] {message}";
+			Console.WriteLine(message);
+		#endif
+		}
 
-        public static void LogMessage(object obj, string message, ELogMessageType messageType = ELogMessageType.Default)
-        {
-            LogMessage($"[{obj.GetType().Name}] {message}", messageType);
-        }
+		public static void LogMessage(object obj, string message, ELogMessageType messageType = ELogMessageType.Default)
+		{
+		#if UNITY_ENGINE
+			var resultMessage = $"[{obj.GetType().Name}] {message}";
+            switch (messageType)
+            {
+                case ELogMessageType.Default:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Info:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Warning:
+                    Debug.LogWarning(resultMessage);
+                    break;
+                case ELogMessageType.Error:
+                    Debug.LogError(resultMessage);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+            }
+		#else
+			LogMessage($"[{obj.GetType().Name}] {message}", messageType);
+		#endif
+		}
 
-        public static void LogObjectMessage(this object obj, string message, ELogMessageType messageType = ELogMessageType.Default)
-        {
-            LogMessage(obj, message, messageType);
-        }
+		public static void LogObjectMessage(this object obj, string message, ELogMessageType messageType = ELogMessageType.Default)
+		{
+		#if UNITY_ENGINE
+			var resultMessage = $"[{obj.GetType().Name}] {message}";
+            switch (messageType)
+            {
+                case ELogMessageType.Default:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Info:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Warning:
+                    Debug.LogWarning(resultMessage);
+                    break;
+                case ELogMessageType.Error:
+                    Debug.LogError(resultMessage);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+            }
+		#else
+			LogMessage($"[{obj.GetType().Name}] {message}", messageType);
+		#endif
+		}
 
-        public static void LogMessage(object obj, string methodName, string message, ELogMessageType messageType = ELogMessageType.Default)
-        {
-            LogMessage(obj, $"[{methodName}] {message}", messageType);
-        }
+		public static void LogMessage(object obj, string methodName, string message, ELogMessageType messageType = ELogMessageType.Default)
+		{
+		#if UNITY_ENGINE
+			var resultMessage = $"[{obj.GetType().Name}] [{methodName}] {message}";
+            switch (messageType)
+            {
+                case ELogMessageType.Default:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Info:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Warning:
+                    Debug.LogWarning(resultMessage);
+                    break;
+                case ELogMessageType.Error:
+                    Debug.LogError(resultMessage);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+            }
+		#else
+			LogMessage($"[{obj.GetType().Name}] [{methodName}] {message}", messageType);
+		#endif
+		}
 
-        public static void LogObjectMessage(this object obj, string methodName, string message, ELogMessageType messageType = ELogMessageType.Default)
-        {
-            LogMessage(obj, methodName, message, messageType);
-        }
-        
-    }
+		public static void LogObjectMessage(this object obj, string methodName, string message, ELogMessageType messageType = ELogMessageType.Default)
+		{
+		#if UNITY_ENGINE
+			var resultMessage = $"[{obj.GetType().Name}] [{methodName}] {message}";
+            switch (messageType)
+            {
+                case ELogMessageType.Default:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Info:
+                    Debug.Log(resultMessage);
+                    break;
+                case ELogMessageType.Warning:
+                    Debug.LogWarning(resultMessage);
+                    break;
+                case ELogMessageType.Error:
+                    Debug.LogError(resultMessage);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+            }
+		#else
+			LogMessage($"[{obj.GetType().Name}] [{methodName}] {message}", messageType);
+		#endif
+		}
+	}
 }
